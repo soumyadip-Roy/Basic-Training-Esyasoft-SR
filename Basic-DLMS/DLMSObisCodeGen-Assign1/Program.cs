4216591a92eaf9ba52e4e55f0816b5ae659b3713
soumyadip-Roy <Soumyadip.Roy@esyasoft.com>
2025-09-17 17:49:49 +0530

    Basic Obis Generation Code

---
+++ Basic-DLMS/DLMSObisCodeGen-Assign1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleAppDLMS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var obis = new ObisCode(1, 0, 1, 8, 0, 255);
+
+            ushort classId = 3;
+            byte attributeId = 2; // e.g., value attribute
+
+            byte[] apdu = GetRequestApdu.CreateGetRequest(obis, classId, attributeId);
+
+            Console.WriteLine("OBIS Code: " + obis);
+            Console.WriteLine("GET Request APDU: " + BitConverter.ToString(apdu));
+        }
+    }
+}
