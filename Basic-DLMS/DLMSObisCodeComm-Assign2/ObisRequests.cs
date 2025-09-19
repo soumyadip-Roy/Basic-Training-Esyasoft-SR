9e3344dd6ae7ee48bb66dd94c9a384d0c37c7bd8
soumyadip-Roy <Soumyadip.Roy@esyasoft.com>
2025-09-19 11:42:51 +0530

    meter Communication Assignment

---
+++ Basic-DLMS/DLMSObisCodeComm-Assign2/ObisRequests.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace DLMS_Cosem_comms
+{
+    public class meterObject
+    {
+        public List<int> obis = new List<int>();
+        public string name;
+        public int value;
+
+        public meterObject(List<int> obisNumList, string Name, int Value)
+        {
+            foreach(int i in obisNumList)
+            {
+                this.obis.Add(i);
+            };
+            this.name = Name;
+            this.value = Value;
+        }
+    }
+
+    public class meterObisObjects
+    {
+        List<meterObject> meterObjectList = new List<meterObject>();
+
+
+        public void  MeterObjectCreator(List<int> obisCode, string Name, int valueObis)
+        {
+            meterObject temp = new meterObject(obisCode, Name, valueObis);
+            meterObjectList.Add(temp);
+            Console.WriteLine("New Obis Code written-" + Name +"-"+valueObis);
+        }
+
+        public meterObject AccessObisObject(int i)
+        {
+            return (meterObjectList[i]);
+        }
+        public int meterObjectCount() { return (meterObjectList.Count); }
+        
+    }
+    public class ObisRequests
+    {    // build class for meter object
+
+        meterObisObjects obisObjects = new meterObisObjects();
+        bool obis_match(List<int> obisCode, List<int> obisCode_DLMS)
+        {
+            bool val = true;
+            for(int i = 0; i < 6; i++)
+            {
+                val = val && (obisCode[i] == obisCode_DLMS[i]);
+            }
+            return (val);
+        }
+        public void dlmsSetOBISCodes(List<int> obisCode, string Name, int value)
+        {
+            obisObjects.MeterObjectCreator(obisCode, Name, value);
+        }
+        public void dlms_get(List<int> obisCode)
+        {
+            for(int i = 0; i < obisObjects.meterObjectCount(); i++)
+            {
+                meterObject obisData = obisObjects.AccessObisObject(i);
+                if (obis_match(obisCode, obisData.obis))
+                {
+                    Console.WriteLine("[GET] " + obisData.name + " = " + obisData.value);
+                    return;
+                }
+            }
+            Console.WriteLine("[GET] OBIS not Found.");
+
+        }
+
+        public void dlms_set(List<int> obisCode, int setValue)
+        {
+            for (int i = 0; i < obisObjects.meterObjectCount(); i++)
+            {
+                meterObject obisData = obisObjects.AccessObisObject(i);
+                if (obis_match(obisCode, obisData.obis))
+                {
+                    obisData.value = setValue;
+                    Console.WriteLine("[SET] Value of " + obisData.name + " Updated to " + obisData.value);
+                    return;
+                }
+            }
+            Console.WriteLine("[SET] OBIS not Found.");
+        }
+
+        public void dlms_action(string actionName)
+        {
+            if (actionName == "RESET")
+            {
+                for (int i = 0; i < obisObjects.meterObjectCount(); i++)
+                {
+                    meterObject obisData = obisObjects.AccessObisObject(i);
+
+                    obisData.value = 0;
+                    Console.WriteLine("[ACTION] Value of " + obisData.name + " Updated to " + obisData.value);
+                }
+                Console.WriteLine("[ACTION] Reset Completed.");
+            }
+
+            else if (actionName == "SYNC_TIME")
+            {
+                Console.WriteLine("ACTION] Synchronizing meter clock to system time...");
+            }
+
+            else { Console.WriteLine("[ACTION] Unknown Method"); }
+        }
+
+
+    }
+}
---
+++ Basic-DLMS/DLMSObisCodeComm-Assign2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DLMS_Cosem_comms
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            ObisRequests obisRequests = new ObisRequests();
+
+            List<int> obisCode_1 = new List<int> { 1, 20, 10, 10, 100, 20 }; // sample obis code 1
+            List<int> obisCode_2 = new List<int> { 1, 21, 1, 1, 101, 21 }; // sample obis code 2
+            List<int> obisCode_3 = new List<int> { 1, 212, 1, 12, 101, 21 }; // sample obis code 3
+
+            obisRequests.dlmsSetOBISCodes(obisCode_1, "obisCode_1", 0);
+            obisRequests.dlmsSetOBISCodes(obisCode_2, "obisCode_2", 0);
+            obisRequests.dlmsSetOBISCodes(obisCode_3, "obisCode_3", 0);
+
+            obisRequests.dlms_get(obisCode_1);
+            obisRequests.dlms_set(obisCode_1, 100);
+            obisRequests.dlms_get(obisCode_1);
+
+            obisRequests.dlms_action("RESET");
+
+        }
+    }
+}
